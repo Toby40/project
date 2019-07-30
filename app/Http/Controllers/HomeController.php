def62822087060faf9c3f1aae2dad230bfc88a62
Psycadelik <Francisadrian9@hotmail.com>
2019-07-30 14:43:43 +0300

    changes to agent dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,28 @@ class HomeController extends Controller
 
     public function agentDashboard()
     {
-        return view('Agent.dashboard');
+        $lands = Land::query()->where('land_status',1)->get()->all();
+
+        return view('Agent.dashboard',compact('lands'));
+    }
+
+    public function getLand($id)
+    {
+        $land = Land::query()->where('land_status',1)->first();
+        return view('Agent.update_land',compact('land'));
+    }
+
+    public function updateLand(Request $request,$id)
+    {
+        $land = Land::query()->where('id',$id)->update([
+            'title_no' => $request->get('title_no'), 
+            'land_size' => $request->get('land_size'), 
+            'land_location' => $request->get('land_location'),
+             'land_price' => $request->get('land_price')
+        ]);
+
+
+        return redirect('/agent/dashboard');
     }
 
     public function staffDashboard()
